4de35c8cbf20741b05a9825ddfd1ddac4e6fb46d
Tom Sherman <the.tomsherman@gmail.com>
2024-01-30 14:32:35 +0000

    Stream!

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { Suspense } from "react";
 import { Client } from "./client";
 
 export default async function Page() {
-  return <Client data={process.platform} />;
+  return (
+    <>
+      <Client data={process.platform} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Delayed />
+      </Suspense>
+    </>
+  );
+}
+
+async function Delayed() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return <p>Delayed</p>;
 }
